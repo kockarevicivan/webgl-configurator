a2ec26afb1048eb9c1c10bef9914496438c952c8
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2023-01-21 21:22:38 +0100

    Tidier code

--- src/js/index.js
+++ src/js/index.js
@@ -63,8 +63,6 @@ mat4.perspective(projectionMatrix,
 
 gl.enable(gl.DEPTH_TEST);
 
-
-
 function cube(vertexData, colorData, uvData, indices, normals, modelMatrix, vertexShader, fragmentShader) {
     const program = initProgram(gl, vertexData, colorData, uvData, indices, normals, vertexShader, fragmentShader);
 
@@ -114,86 +112,6 @@ function model(mesh, modelMatrix, vertexShader, fragmentShader) {
         model: gl.getUniformLocation(program, "model"),
         view: gl.getUniformLocation(program, "view"),
         projection: gl.getUniformLocation(program, "projection"),
-        textureID: gl.getUniformLocation(program, 'textureID'),
-    };
-
-    gl.uniform1i(uniformLocations.textureID, 0);
-
-    // Move box
-    mat4.rotateX(modelMatrix, modelMatrix, Math.PI / -180);
-    mat4.rotateY(modelMatrix, modelMatrix, Math.PI / 90);
-    mat4.rotateZ(modelMatrix, modelMatrix, Math.PI / 270);
-    mat4.translate(modelMatrix, modelMatrix, [0, 0, 0.03]);
-
-    // M+V
-    mat4.multiply(mvMatrix, viewMatrix, modelMatrix);
-    // M+V+P
-    mat4.multiply(mvpMatrix, projectionMatrix, mvMatrix);
-
-    gl.uniformMatrix4fv(uniformLocations.matrix, false, mvpMatrix);
-    gl.uniformMatrix4fv(uniformLocations.model, false, modelMatrix);
-    gl.uniformMatrix4fv(uniformLocations.view, false, viewMatrix);
-    gl.uniformMatrix4fv(uniformLocations.projection, false, projectionMatrix);
-
-    function drawMesh(mesh, shaderProgram) {
-        // make sure you have vertex, vertex normal, and texture coordinate
-        // attributes located in your shaders and attach them to the shader program
-        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "position");
-        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
-        shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "normal");
-        gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
-
-        shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "uv");
-        gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
-
-        // create and initialize the vertex, vertex normal, and texture coordinate buffers
-        // and save on to the mesh object
-        initMeshBuffers(gl, mesh);
-
-        // now to render the mesh
-        gl.bindBuffer(gl.ARRAY_BUFFER, mesh.vertexBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mesh.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-        // it's possible that the mesh doesn't contain
-        // any texture coordinates (e.g. suzanne.obj in the development branch).
-        // in this case, the texture vertexAttribArray will need to be disabled
-        // before the call to drawElements
-        if (!mesh.textures.length) {
-            gl.disableVertexAttribArray(shaderProgram.textureCoordAttribute);
-        }
-        else {
-            // if the texture vertexAttribArray has been previously
-            // disabled, then it needs to be re-enabled
-            gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
-            gl.bindBuffer(gl.ARRAY_BUFFER, mesh.textureBuffer);
-            gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, mesh.textureBuffer.itemSize, gl.FLOAT, false, 0, 0);
-        }
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, mesh.normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer);
-        gl.drawElements(gl.TRIANGLES, mesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-    }
-
-    drawMesh(mesh, program);
-}
-
-function modelPhong(mesh, modelMatrix) {
-    const program = gl.createProgram();
-
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, textureFragmentShaderPhong);
-
-    gl.linkProgram(program);
-    gl.useProgram(program);
-
-    const uniformLocations = {
-        matrix: gl.getUniformLocation(program, "matrix"),
-        model: gl.getUniformLocation(program, "model"),
-        view: gl.getUniformLocation(program, "view"),
-        projection: gl.getUniformLocation(program, "projection"),
         textureID: gl.getUniformLocation(program, "textureID"),
         cameraPosition: gl.getUniformLocation(program, "cameraPosition"),
         light: gl.getUniformLocation(program, "light"),
@@ -202,11 +120,6 @@ function modelPhong(mesh, modelMatrix) {
     };
 
     gl.uniform1i(uniformLocations.textureID, 0);
-
-    // Move box
-    mat4.rotateX(modelMatrix, modelMatrix, Math.PI / -180);
-    mat4.rotateY(modelMatrix, modelMatrix, Math.PI / 90);
-    mat4.rotateZ(modelMatrix, modelMatrix, Math.PI / 270);
 
     // M+V
     mat4.multiply(mvMatrix, viewMatrix, modelMatrix);
@@ -268,7 +181,6 @@ function modelPhong(mesh, modelMatrix) {
     drawMesh(mesh, program);
 }
 
-
 // Move camera (invert missing)
 mat4.translate(viewMatrix, viewMatrix, [0, 0, -10]);
 
@@ -276,12 +188,12 @@ function animate() {
     requestAnimationFrame(animate);
 
     cube(vertexData, colorData, uvData, indices, normals, colorCubeModelMatrix, vertexShader, colorFragmentShader);
-    
+
     mat4.rotateX(colorCubeModelMatrix, colorCubeModelMatrix, Math.PI / -180);
     mat4.rotateY(colorCubeModelMatrix, colorCubeModelMatrix, Math.PI / -90);
     mat4.rotateZ(colorCubeModelMatrix, colorCubeModelMatrix, Math.PI / 270);
     mat4.translate(colorCubeModelMatrix, colorCubeModelMatrix, [0, 0, -0.03]);
-    
+
     cube(vertexData, colorData, uvData, indices, normals, textureCubeModelMatrix, vertexShader, textureFragmentShader);
     mat4.rotateX(textureCubeModelMatrix, textureCubeModelMatrix, Math.PI / 180);
     mat4.rotateY(textureCubeModelMatrix, textureCubeModelMatrix, Math.PI / 90);
@@ -294,7 +206,15 @@ function animate() {
 
     if (state.meshes.model) {
         model(state.meshes.model, modelCubeModelMatrix, vertexShader, textureFragmentShader);
-        modelPhong(state.meshes.model, modelCubeModelPhongMatrix, vertexShader, textureFragmentShaderPhong);
+        mat4.rotateX(modelCubeModelMatrix, modelCubeModelMatrix, Math.PI / -180);
+        mat4.rotateY(modelCubeModelMatrix, modelCubeModelMatrix, Math.PI / 90);
+        mat4.rotateZ(modelCubeModelMatrix, modelCubeModelMatrix, Math.PI / 270);
+        mat4.translate(modelCubeModelMatrix, modelCubeModelMatrix, [0, 0, 0.03]);
+
+        model(state.meshes.model, modelCubeModelPhongMatrix, vertexShader, textureFragmentShaderPhong);
+        mat4.rotateX(modelCubeModelPhongMatrix, modelCubeModelPhongMatrix, Math.PI / -180);
+        mat4.rotateY(modelCubeModelPhongMatrix, modelCubeModelPhongMatrix, Math.PI / 90);
+        mat4.rotateZ(modelCubeModelPhongMatrix, modelCubeModelPhongMatrix, Math.PI / 270);
     }
 }
 
